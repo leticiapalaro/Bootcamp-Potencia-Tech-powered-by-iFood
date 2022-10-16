03f368bdb50852c933f5321d66a48a9ef9498654
Letícia Palaro <109923860+leticiapalaro@users.noreply.github.com>
2022-10-16 15:29:31 -0400

    Update Desafios

--- exercicios-e-desafios/src/estudandojava/bootcamp/desafios/modulo7/FatorialDesajeitado.java
+++ exercicios-e-desafios/src/estudandojava/bootcamp/desafios/modulo7/FatorialDesajeitado.java
@@ -1,50 +1,53 @@
 package estudandojava.bootcamp.desafios.modulo7;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import static java.lang.System.out;
-
 public class FatorialDesajeitado {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
         //TODO: Cálculo do valor do Fatorial Desajeitado de "numero".
 
+
         int numero = new Scanner(System.in).nextInt();
-        int resultado = 0; // variável que irá armazenar o resultado
-        int tmp = numero; // variável temporária para armazenar valores
-        numero--;
+        int temp = 0;
+        int resultado = 0;
 
-        while (numero > 0) {
 
-            tmp *= numero;
-            numero--;
+        //Lista que irá armazenar números para somar depois:
+        List<Integer> listaNumeros = new ArrayList<>();
 
-            if(numero != 0){ // evitando divisão por zero
-                tmp /= numero;
-                numero--;
-            }
 
-            tmp += numero;
-            numero--;
+        //Quebrando o Fatorial Desajeitado em partes menores e armazenando na lista:
+        while (numero >= 3) {
+            temp = numero * (numero - 1) / (numero - 2);
+            numero -= 3;
 
-            resultado += tmp;
+            if (listaNumeros.isEmpty()){
+                listaNumeros.add(temp);
+            }else{
+                listaNumeros.add(temp*(-1)); // Nessa parte, se não for o primeiro ciclo do while, seguindo a ordem
+            }                                // dos operadores, irá ter um sinal de negativo, por isso o (temp*(-1)).
 
-            tmp = -numero;
-
-            /*
-             * Se sobrar apenas 1 na variável número,
-             * como a próxima rotação é a multiplicação
-             * o resultado será o mesmo até este ponto.
-             * Então o resultado final já será vinculadado.
-             */
-
-            if (numero == 1 ){
-                resultado += tmp;
-                break;
-            }
-
+            listaNumeros.add(numero);
             numero--;
         }
 
+        if (numero == 2){
+            temp = numero * (numero - 1);
+            listaNumeros.add(temp*(-1)); /* Seguindo a ordem dos operadores,
+             * aqui tem um sinal de negativo, por isso o (temp*(-1)). */
+        }else if (numero == 1){
+            listaNumeros.add(numero*(-1)); // Seguindo a ordem dos operadores,
+        }                                  // aqui tem um sinal de negativo, por isso o (temp*(-1)).
+
+
+        // Somando todos os números dentro da lista:
+        for (int n : listaNumeros) resultado+= n;
+
+
+        // Resultado:
         out.println(resultado);
     }
 }
